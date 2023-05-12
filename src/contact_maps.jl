69447f880067e3c182d738bb98a5715b0809eb93
Jorge Fernandez-de-Cossio-Diaz <cossio@users.noreply.github.com>
2023-05-12 17:09:55 +0200

    A, B

--- src/contact_maps.jl
+++ src/contact_maps.jl
@@ -5,3 +5,9 @@ end
 
 const CONTACT_MAPS = load_contact_maps()
 const N_STRUCTURES = size(CONTACT_MAPS, 2)
+
+# index of some structure
+const CONTACT_MAP_A = 625
+const CONTACT_MAP_B = 848
+const CONTACT_MAP_C = 6683
+const CONTACT_MAP_D = 6685
